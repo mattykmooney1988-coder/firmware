f71e28cb18f73fb3d911e6cee54bdd5068f52747
mattykmooney1988-coder <mattykmooney1988@gmail.com>
2026-07-16 02:21:21 +0100

    Fix FNK0103 display config for ST7789 240x320 board

--- boards/fnk0103/interface.cpp
+++ boards/fnk0103/interface.cpp
@@ -1,107 +1,91 @@
-#include "core/powerSave.h"
-#include "core/utils.h"
-#include <interface.h>
-
-#include <CYD28_TouchscreenR.h>
-CYD28_TouchR touch(TFT_HEIGHT, TFT_WIDTH);
-
-/***************************************************************************************
-** Function name: _setup_gpio()
-** Location: main.cpp
-** Description:   initial setup for the device
-***************************************************************************************/
-void _setup_gpio() {
-    pinMode(XPT2046_SPI_CONFIG_CS_GPIO_NUM, OUTPUT);
-    digitalWrite(XPT2046_SPI_CONFIG_CS_GPIO_NUM, HIGH);
-    bruceConfig.colorInverted = 0;
-}
-
-/***************************************************************************************
-** Function name: _post_setup_gpio()
-** Location: main.cpp
-** Description:   second stage gpio setup to make a few functions work
-***************************************************************************************/
-void _post_setup_gpio() {
-    if (!touch.begin(&tft.getSPIinstance())) {
-        Serial.println("Touch IC not Started");
-        log_i("Touch IC not Started");
-    } else Serial.println("Touch IC Started");
-
-    pinMode(TFT_BL, OUTPUT);
-    ledcAttach(TFT_BL, TFT_BRIGHT_FREQ, TFT_BRIGHT_Bits);
-    ledcWrite(TFT_BL, 255);
-}
-
-/***************************************************************************************
-** Function name: getBattery()
-** location: display.cpp
-** Description:   Delivers the battery value from 1-100
-***************************************************************************************/
-int getBattery() { return 0; }
-
-/*********************************************************************
-** Function: setBrightness
-** location: settings.cpp
-** set brightness value
-**********************************************************************/
-void _setBrightness(uint8_t brightval) {
-    int dutyCycle;
-    if (brightval == 100) dutyCycle = 255;
-    else if (brightval == 75) dutyCycle = 130;
-    else if (brightval == 50) dutyCycle = 70;
-    else if (brightval == 25) dutyCycle = 20;
-    else if (brightval == 0) dutyCycle = 0;
-    else dutyCycle = ((brightval * 255) / 100);
-    ledcWrite(TFT_BL, dutyCycle);
-}
-
-/*********************************************************************
-** Function: InputHandler
-** Handles the variables PrevPress, NextPress, SelPress, AnyKeyPress and EscPress
-**********************************************************************/
-void InputHandler(void) {
-    static unsigned long tm = millis();
-    if (!(millis() - tm > 200 || LongPress)) return;
-
-    if (touch.touched()) {
-        auto t = touch.getPointScaled();
-        t = touch.getPointScaled();
-        if (bruceConfigPins.rotation == 3) {
-            t.y = (tftHeight + 20) - t.y;
-            t.x = tftWidth - t.x;
-        }
-        if (bruceConfigPins.rotation == 0) {
-            int tmp = t.x;
-            t.x = tftWidth - t.y;
-            t.y = tmp;
-        }
-        if (bruceConfigPins.rotation == 2) {
-            int tmp = t.x;
-            t.x = t.y;
-            t.y = (tftHeight + 20) - tmp;
-        }
-        tm = millis();
-        if (!wakeUpScreen()) AnyKeyPress = true;
-        else return;
-        touchPoint.x = t.x;
-        touchPoint.y = t.y;
-        touchPoint.pressed = true;
-        touchHeatMap(touchPoint);
-    } else touchPoint.pressed = false;
-}
-
-/*********************************************************************
-** Function: powerOff
-** location: mykeyboard.cpp
-** Turns off the device (or try to)
-**********************************************************************/
-void powerOff() {
-    esp_deep_sleep_start();
-}
-
-/*********************************************************************
-** Function: checkReboot
-** location: mykeyboard.cpp
-** Btn logic to turn off the device (name is odd btw)
-**********************************************************************/
-void checkReboot() {}
+1| #include "core/powerSave.h"
+2| #include "core/utils.h"
+3| #include <interface.h>
+4| 
+5| #include <CYD28_TouchscreenR.h>
+6| CYD28_TouchR touch(TFT_HEIGHT, TFT_WIDTH);
+7| 
+8| /***************************************************************************************
+9| ** Function name: _setup_gpio()
+10| ** Location: main.cpp
+11| ** Description:   initial setup for the device
+12| ***************************************************************************************/
+13| void _setup_gpio() {
+14|     pinMode(XPT2046_SPI_CONFIG_CS_GPIO_NUM, OUTPUT);
+15|     digitalWrite(XPT2046_SPI_CONFIG_CS_GPIO_NUM, HIGH);
+16|     bruceConfig.colorInverted = 0;
+17| }
+18| 
+19| /***************************************************************************************
+20| ** Function name: _post_setup_gpio()
+21| ** Location: main.cpp
+22| ** Description:   second stage gpio setup to make a few functions work
+23| ***************************************************************************************/
+24| void _post_setup_gpio() {
+25|     if (!touch.begin(&tft.getSPIinstance())) {
+26|         Serial.println("Touch IC not Started");
+27|         log_i("Touch IC not Started");
+28|     } else Serial.println("Touch IC Started");
+29| 
+30|     pinMode(TFT_BL, OUTPUT);
+31|     ledcAttach(TFT_BL, TFT_BRIGHT_FREQ, TFT_BRIGHT_Bits);
+32|     ledcWrite(TFT_BL, 255);
+33| }
+34| 
+35| /***************************************************************************************
+36| ** Function name: getBattery()
+37| ** location: display.cpp
+38| ** Description:   Delivers the battery value from 1-100
+39| ***************************************************************************************/
+40| int getBattery() { return 0; }
+41| 
+42| /*********************************************************************
+43| ** Function: setBrightness
+44| ** location: settings.cpp
+45| ** set brightness value
+46| **********************************************************************/
+47| void _setBrightness(uint8_t brightval) {
+48|     int dutyCycle;
+49|     if (brightval == 100) dutyCycle = 255;
+50|     else if (brightval == 75) dutyCycle = 130;
+51|     else if (brightval == 50) dutyCycle = 70;
+52|     else if (brightval == 25) dutyCycle = 20;
+53|     else if (brightval == 0) dutyCycle = 0;
+54|     else dutyCycle = ((brightval * 255) / 100);
+55|     ledcWrite(TFT_BL, dutyCycle);
+56| }
+57| 
+58| /*********************************************************************
+59| ** Function: InputHandler
+60| ** Handles the variables PrevPress, NextPress, SelPress, AnyKeyPress and EscPress
+61| **********************************************************************/
+62| void InputHandler(void) {
+63|     static unsigned long tm = millis();
+64|     if (!(millis() - tm > 200 || LongPress)) return;
+65| 
+66|     if (touch.touched()) {
+67|         auto t = touch.getPointScaled();
+68|         t = touch.getPointScaled();
+69|         if (bruceConfigPins.rotation == 3) {
+70|             t.y = (tftHeight + 20) - t.y;
+71|             t.x = tftWidth - t.x;
+72|         }
+73|         if (bruceConfigPins.rotation == 0) {
+74|             int tmp = t.x;
+75|             t.x = tftWidth - t.y;
+76|             t.y = tmp;
+77|         }
+78|         if (bruceConfigPins.rotation == 2) {
+79|             int tmp = t.x;
+80|             t.x = t.y;
+81|             t.y = (tftHeight + 20) - tmp;
+82|         }
+83|         tm = millis();
+84|         if (!wakeUpScreen()) AnyKeyPress = true;
+85|         else return;
+86|         touchPoint.x = t.x;
+87|         touchPoint.y = t.y;
+88|         touchPoint.pressed = true;
+89|         touchHeatMap(touchPoint);
+90|     } else touchPoint.pressed = false;
+91| }
--- boards/fnk0103/pins_arduino.h
+++ boards/fnk0103/pins_arduino.h
@@ -1,136 +1,123 @@
-#ifndef Pins_Arduino_h
-#define Pins_Arduino_h
-
-#include "soc/soc_caps.h"
-#include <stdint.h>
-
-// USB Serial (ESP32-S3 native USB)
-static const uint8_t TX = 43;
-static const uint8_t RX = 44;
-
-// Main SPI bus (shared with SD card, CC1101, NRF24)
-#define SPI_SCK_PIN  12
-#define SPI_MOSI_PIN 11
-#define SPI_MISO_PIN 13
-#define SPI_SS_PIN   15
-
-static const uint8_t SS   = SPI_SS_PIN;
-static const uint8_t MOSI = SPI_MOSI_PIN;
-static const uint8_t MISO = SPI_MISO_PIN;
-static const uint8_t SCK  = SPI_SCK_PIN;
-
-// I2C / Grove
-#define GROVE_SDA 4
-#define GROVE_SCL 5
-static const uint8_t SDA = GROVE_SDA;
-static const uint8_t SCL = GROVE_SCL;
-
-// Serial / GPS
-#define SERIAL_TX     43
-#define SERIAL_RX     44
-#define GPS_SERIAL_TX SERIAL_TX
-#define GPS_SERIAL_RX SERIAL_RX
-
-// BadUSB
-#define BAD_TX SERIAL_TX
-#define BAD_RX SERIAL_RX
-#define USB_as_HID 1
-
-// Buttons - touch only
-#define HAS_BTN    0
-#define BTN_ALIAS  "\"Ok\""
-#define BTN_PIN    0
-#define BTN_ACT    LOW
-
-// IR / RF
-#define TXLED   2
-#define LED_ON  HIGH
-#define LED_OFF LOW
-
-// CC1101 on main SPI bus
-#define USE_CC1101_VIA_SPI
-#define CC1101_GDO0_PIN  2
-#define CC1101_SS_PIN    15
-#define CC1101_MOSI_PIN  SPI_MOSI_PIN
-#define CC1101_SCK_PIN   SPI_SCK_PIN
-#define CC1101_MISO_PIN  SPI_MISO_PIN
-
-// NRF24 on main SPI bus
-#define USE_NRF24_VIA_SPI
-#define NRF24_CE_PIN    2
-#define NRF24_SS_PIN    15
-#define NRF24_MOSI_PIN  SPI_MOSI_PIN
-#define NRF24_SCK_PIN   SPI_SCK_PIN
-#define NRF24_MISO_PIN  SPI_MISO_PIN
-
-// W5500 on main SPI bus
-#define USE_W5500_VIA_SPI
-#define W5500_SS_PIN    15
-#define W5500_MOSI_PIN  SPI_MOSI_PIN
-#define W5500_SCK_PIN   SPI_SCK_PIN
-#define W5500_MISO_PIN  SPI_MISO_PIN
-#define W5500_INT_PIN   2
-
-// SD Card on main SPI bus
-#define SDCARD_CS   42
-#define SDCARD_SCK  SPI_SCK_PIN
-#define SDCARD_MISO SPI_MISO_PIN
-#define SDCARD_MOSI SPI_MOSI_PIN
-
-// Font sizes
-#define FP 1
-#define FM 2
-#define FG 3
-
-// Screen / Display
-#define HAS_SCREEN 1
-#define ROTATION   1
-#define BACKLIGHT  45
-#define MINBRIGHT  (uint8_t)1
-
-// TFT_eSPI - ST7789, 3.2" 320x240, SPI
-#define USER_SETUP_LOADED 1
-#define ST7789_DRIVER     1
-#define TFT_WIDTH         240
-#define TFT_HEIGHT        320
-#define TFT_MISO          13
-#define TFT_MOSI          11
-#define TFT_SCLK          12
-#define TFT_CS            10
-#define TFT_DC            9
-#define TFT_RST           46
-#define TFT_BL            45
-#define TFT_BACKLIGHT_ON  HIGH
-#define SMOOTH_FONT       1
-#define SPI_FREQUENCY       40000000
-#define SPI_READ_FREQUENCY  20000000
-#define SPI_TOUCH_FREQUENCY 2500000
-
-// Resistive touch (XPT2046, shared SPI bus)
-#define HAS_TOUCH              1
-#define HAS_RESISTIVE_TOUCH    1
-#define TOUCH_XPT2046_SPI
-#define XPT2046_TOUCH_CONFIG_INT_GPIO_NUM  -1
-#define XPT2046_SPI_BUS_MISO_IO_NUM        SPI_MISO_PIN
-#define XPT2046_SPI_BUS_MOSI_IO_NUM        SPI_MOSI_PIN
-#define XPT2046_SPI_BUS_SCLK_IO_NUM        SPI_SCK_PIN
-#define XPT2046_SPI_CONFIG_CS_GPIO_NUM     14
-
-// TFT brightness PWM
-#define TFT_BRIGHT_CHANNEL 0
-#define TFT_BRIGHT_Bits    8
-#define TFT_BRIGHT_FREQ    5000
-
-// Audio / Mic - not present
-#define PIN_CLK       -1
-#define PIN_DATA      -1
-#define I2S_SCLK_PIN  -1
-#define I2S_DATA_PIN  -1
-#define BCLK          -1
-#define WCLK          -1
-#define DOUT          -1
-
-// RGB LED - not present
-#define RGB_LED -1
-
-#endif /* Pins_Arduino_h */
+1| #ifndef Pins_Arduino_h
+2| #define Pins_Arduino_h
+3| 
+4| #include "soc/soc_caps.h"
+5| #include <stdint.h>
+6| 
+7| // USB Serial (ESP32-S3 native USB)
+8| static const uint8_t TX = 43;
+9| static const uint8_t RX = 44;
+10| 
+11| // Main SPI bus (shared with SD card, CC1101, NRF24)
+12| #define SPI_SCK_PIN  12
+13| #define SPI_MOSI_PIN 11
+14| #define SPI_MISO_PIN 13
+15| #define SPI_SS_PIN   15
+16| 
+17| static const uint8_t SS   = SPI_SS_PIN;
+18| static const uint8_t MOSI = SPI_MOSI_PIN;
+19| static const uint8_t MISO = SPI_MISO_PIN;
+20| static const uint8_t SCK  = SPI_SCK_PIN;
+21| 
+22| // I2C / Grove
+23| #define GROVE_SDA 4
+24| #define GROVE_SCL 5
+25| static const uint8_t SDA = GROVE_SDA;
+26| static const uint8_t SCL = GROVE_SCL;
+27| 
+28| // Serial / GPS
+29| #define SERIAL_TX     43
+30| #define SERIAL_RX     44
+31| 
+32| // IR / RF
+33| #define TXLED   2
+34| #define LED_ON  HIGH
+35| #define LED_OFF LOW
+36| 
+37| // CC1101 on main SPI bus
+38| #define USE_CC1101_VIA_SPI
+39| #define CC1101_GDO0_PIN  2
+40| #define CC1101_SS_PIN    15
+41| #define CC1101_MOSI_PIN  SPI_MOSI_PIN
+42| #define CC1101_SCK_PIN   SPI_SCK_PIN
+43| #define CC1101_MISO_PIN  SPI_MISO_PIN
+44| 
+45| // NRF24 on main SPI bus
+46| #define USE_NRF24_VIA_SPI
+47| #define NRF24_CE_PIN    2
+48| #define NRF24_SS_PIN    15
+49| #define NRF24_MOSI_PIN  SPI_MOSI_PIN
+50| #define NRF24_SCK_PIN   SPI_SCK_PIN
+51| #define NRF24_MISO_PIN  SPI_MISO_PIN
+52| 
+53| // W5500 on main SPI bus
+54| #define USE_W5500_VIA_SPI
+55| #define W5500_SS_PIN    15
+56| #define W5500_MOSI_PIN  SPI_MOSI_PIN
+57| #define W5500_SCK_PIN   SPI_SCK_PIN
+58| #define W5500_MISO_PIN  SPI_MISO_PIN
+59| #define W5500_INT_PIN   2
+60| 
+61| // SD Card on main SPI bus
+62| #define SDCARD_CS   42
+63| #define SDCARD_SCK  SPI_SCK_PIN
+64| #define SDCARD_MISO SPI_MISO_PIN
+65| #define SDCARD_MOSI SPI_MOSI_PIN
+66| 
+67| // Font sizes
+68| #define FP 1
+69| #define FM 2
+70| #define FG 3
+71| 
+72| // Screen / Display
+73| #define HAS_SCREEN 1
+74| #define ROTATION   1
+75| #define BACKLIGHT  45
+76| #define MINBRIGHT  (uint8_t)1
+77| 
+78| // TFT_eSPI - ST7789, 3.2" 320x240, SPI
+79| #define USER_SETUP_LOADED 1
+80| #define ST7789_DRIVER     1
+81| #define TFT_WIDTH         240
+82| #define TFT_HEIGHT        320
+83| #define TFT_MISO          13
+84| #define TFT_MOSI          11
+85| #define TFT_SCLK          12
+86| #define TFT_CS            10
+87| #define TFT_DC            9
+88| #define TFT_RST           46
+89| #define TFT_BL            45
+90| #define TFT_BACKLIGHT_ON  HIGH
+91| #define SMOOTH_FONT       1
+92| #define SPI_FREQUENCY       40000000
+93| #define SPI_READ_FREQUENCY  20000000
+94| #define SPI_TOUCH_FREQUENCY 2500000
+95| 
+96| // Resistive touch (XPT2046, shared SPI bus)
+97| #define HAS_TOUCH              1
+98| #define HAS_RESISTIVE_TOUCH    1
+99| #define TOUCH_XPT2046_SPI
+100| #define XPT2046_TOUCH_CONFIG_INT_GPIO_NUM  -1
+101| #define XPT2046_SPI_BUS_MISO_IO_NUM        SPI_MISO_PIN
+102| #define XPT2046_SPI_BUS_MOSI_IO_NUM        SPI_MOSI_PIN
+103| #define XPT2046_SPI_BUS_SCLK_IO_NUM        SPI_SCK_PIN
+104| #define XPT2046_SPI_CONFIG_CS_GPIO_NUM     14
+105| 
+106| // TFT brightness PWM
+107| #define TFT_BRIGHT_CHANNEL 0
+108| #define TFT_BRIGHT_Bits    8
+109| #define TFT_BRIGHT_FREQ    5000
+110| 
+111| // Audio / Mic - not present
+112| #define PIN_CLK       -1
+113| #define PIN_DATA      -1
+114| #define I2S_SCLK_PIN  -1
+115| #define I2S_DATA_PIN  -1
+116| #define BCLK          -1
+117| #define WCLK          -1
+118| #define DOUT          -1
+119| 
+120| // RGB LED - not present
+121| #define RGB_LED -1
+122| 
+123| #endif /* Pins_Arduino_h */
